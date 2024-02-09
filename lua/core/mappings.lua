d705736e6daa6994cc18a03286f34f62aa6577b6
Evgeniy Yde <ydeevg@gmail.com>
2024-02-09 20:11:22 +0300

    add makescripts

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -17,7 +17,8 @@ vim.keymap.set('n', '<leader>x', ':BufferLinePickClose<CR>')
 vim.keymap.set('n', '<leader>X', ':BufferLineCloseRight<CR>')
 vim.keymap.set('n', '<leader>s', ':BufferLineSortByTabs<CR>')
 vim.keymap.set('n', '<leader>=', ':vertical resize +10<CR>')
-vim.keymap.set('n', '<leader>-', ':vertical resize -10<CR>')
+vim.keymap.set('n', '<leader>_', ':horizontal resize -10<CR>')
+vim.keymap.set('n', '<leader>+', ':horizontal resize +10<CR>')
 
 -- Tabs
 vim.keymap.set('n', '<Tab>', ':BufferLineCycleNext<CR>')
@@ -27,4 +28,11 @@ vim.keymap.set('n', '<s-Tab>', ':BufferLineCyclePrev<CR>')
 vim.keymap.set('n', '<leader>tf', ':ToggleTerm direction=float<CR>')
 vim.keymap.set('n', '<leader>tt', ':ToggleTerm direction=horizontal<CR>')
 vim.keymap.set('n', '<leader>tv', ':ToggleTerm direction=vertical size=40<CR>')
+
+-- Make
+vim.keymap.set('n', '<F7>', ':wall | make all<Cr>')
+vim.keymap.set('n', '<F8>', ':make run<Cr>')
+vim.keymap.set('n', '<F4>', ':1000cp<Cr>zvzz:cc<Cr>')
+vim.keymap.set('n', '<F5>', ':cp<Cr>zvzz:cc<Cr>')
+vim.keymap.set('n', '<F6>', ':cn<Cr>zvzz:cc<Cr>')
 
