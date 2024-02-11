a538a7570c3e64a7edea1a1457d7e85ccff1e724
Evgeniy Yde <ydeevg@gmail.com>
2024-02-11 15:41:58 +0300

    chore(neotree): show hidden files

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -8,4 +8,12 @@ vim.fn.sign_define("DiagnosticSignInfo",
 vim.fn.sign_define("DiagnosticSignHint",
   {text = "󰌵", texthl = "DiagnosticSignHint"})
 
-require("neo-tree").setup({})
+require("neo-tree").setup({
+  filesystem = {
+    filtered_items = {
+      visible = true,
+      hide_dotfiles = false,
+      hide_gitignored = true,
+    }
+  }
+})
